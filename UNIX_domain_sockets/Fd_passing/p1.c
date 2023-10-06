7a50f0a5b1ee3beb50829cbc15c3167aaa0f8648
Jahnavi315 <jahnavikanukuntla2003@gmail.com>
2023-10-06 12:33:51 +0530

    debug 1

--- UNIX_domain_sockets/Fd_passing/p1.c
+++ UNIX_domain_sockets/Fd_passing/p1.c
@@ -8,42 +8,6 @@
 #define PATH "p1address"
 #define DEST "p2address"
 
-int send_fd(int socket, int fd_to_send)
- {
-  struct msghdr socket_message;
-  struct iovec io_vector[1];
-  struct cmsghdr *control_message = NULL;
-  char message_buffer[1];
-  /* storage space needed for an ancillary element with a paylod of length is CMSG_SPACE(sizeof(length)) */
-  char ancillary_element_buffer[CMSG_SPACE(sizeof(int))];
-  int available_ancillary_element_buffer_space;
-
-  /* at least one vector of one byte must be sent */
-  message_buffer[0] = 'F';
-  io_vector[0].iov_base = message_buffer;
-  io_vector[0].iov_len = 1;
-
-  /* initialize socket message */
-  memset(&socket_message, 0, sizeof(struct msghdr));
-  socket_message.msg_iov = io_vector;
-  socket_message.msg_iovlen = 1;
-
-  /* provide space for the ancillary data */
-  available_ancillary_element_buffer_space = CMSG_SPACE(sizeof(int));
-  memset(ancillary_element_buffer, 0, available_ancillary_element_buffer_space);
-  socket_message.msg_control = ancillary_element_buffer;
-  socket_message.msg_controllen = available_ancillary_element_buffer_space;
-
-  /* initialize a single ancillary data element for fd passing */
-  control_message = CMSG_FIRSTHDR(&socket_message);
-  control_message->cmsg_level = SOL_SOCKET;
-  control_message->cmsg_type = SCM_RIGHTS;
-  control_message->cmsg_len = CMSG_LEN(sizeof(int));
-  *((int *) CMSG_DATA(control_message)) = fd_to_send;
-
-  return sendmsg(socket, &socket_message, 0);
- }
-
 int main(){
 	int fd=open("temp.txt",O_RDONLY);
 	int usfd=socket(AF_UNIX,SOCK_DGRAM,0);
@@ -57,10 +21,7 @@ int main(){
 	if(bind(usfd,(struct sockaddr*)&addr,sizeof addr)==-1){
 		perror("bind ");
 	}
-	int st = send_fd(usfd,fd);
-	perror("send ");
-	printf("status %i ",st);
-	/*struct iovec e = {NULL,0};
+	struct iovec e = {NULL,0};
 	 char cmsg[CMSG_SPACE(sizeof(int))];
 	struct sockaddr_un destaddr;
 	memset(&destaddr,0,sizeof destaddr);
@@ -73,5 +34,5 @@ int main(){
 	 c->cmsg_len = CMSG_LEN(sizeof(int));
 	 *(int*)CMSG_DATA(c) = fd;
 	 sendmsg(usfd, &m, 0);
-	 perror("send ");*/
+	 perror("send ");
 }
--- UNIX_domain_sockets/Fd_passing/p2.c
+++ UNIX_domain_sockets/Fd_passing/p2.c
@@ -7,60 +7,6 @@
 #include<fcntl.h>
 #define PATH "p2address"
 
-int recv_fd(int socket)
- {
-  int sent_fd, available_ancillary_element_buffer_space;
-  struct msghdr socket_message;
-  struct iovec io_vector[1];
-  struct cmsghdr *control_message = NULL;
-  char message_buffer[1];
-  char ancillary_element_buffer[CMSG_SPACE(sizeof(int))];
-
-  /* start clean */
-  memset(&socket_message, 0, sizeof(struct msghdr));
-  memset(ancillary_element_buffer, 0, CMSG_SPACE(sizeof(int)));
-
-  /* setup a place to fill in message contents */
-  io_vector[0].iov_base = message_buffer;
-  io_vector[0].iov_len = 1;
-  socket_message.msg_iov = io_vector;
-  socket_message.msg_iovlen = 1;
-
-  /* provide space for the ancillary data */
-  socket_message.msg_control = ancillary_element_buffer;
-  socket_message.msg_controllen = CMSG_SPACE(sizeof(int));
-
-  if(recvmsg(socket, &socket_message, MSG_CMSG_CLOEXEC) < 0)
-   return -1;
-
-  if(message_buffer[0] != 'F')
-  {
-   /* this did not originate from the above function */
-   return -1;
-  }
-
-  if((socket_message.msg_flags & MSG_CTRUNC) == MSG_CTRUNC)
-  {
-   /* we did not provide enough space for the ancillary element array */
-   return -1;
-  }
-
-  /* iterate ancillary elements */
-   for(control_message = CMSG_FIRSTHDR(&socket_message);
-       control_message != NULL;
-       control_message = CMSG_NXTHDR(&socket_message, control_message))
-  {
-   if( (control_message->cmsg_level == SOL_SOCKET) &&
-       (control_message->cmsg_type == SCM_RIGHTS) )
-   {
-    sent_fd = *((int *) CMSG_DATA(control_message));
-    return sent_fd;
-   }
-  }
-
-  return -1;
- }
-
 int main(){
 	int usfd=socket(AF_UNIX,SOCK_DGRAM,0);
 	struct sockaddr_un addr;
@@ -73,11 +19,7 @@ int main(){
 	if(bind(usfd,(struct sockaddr*)&addr,sizeof addr)==-1){
 		perror("bind ");
 	}
-	
-	int fd=recv_fd(usfd);
-	printf("rcvd fd %i\n",fd);
-	
-	/*char buf[512];
+	char buf[512];
 	struct iovec e = {buf, 512};
 	char cmsg[CMSG_SPACE(sizeof(int))];
 	struct msghdr m = {NULL, 0, &e, 1, cmsg, sizeof(cmsg), 0};
@@ -85,5 +27,4 @@ int main(){
 	printf("Receive: %d\n", n);
 	struct cmsghdr *c = CMSG_FIRSTHDR(&m);
 	int fd = *(int*)CMSG_DATA(c);
-	*/
 }
