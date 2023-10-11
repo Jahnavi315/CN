61c2793590a93d2727e5424fc4dc87e6368fbb14
Jahnavi315 <jahnavikanukuntla2003@gmail.com>
2023-10-11 15:42:30 +0530

    stream sockets fd sharing

--- UNIX_domain_sockets/Fd_passing/p1.c
+++ UNIX_domain_sockets/Fd_passing/p1.c
@@ -11,6 +11,53 @@
 #define DEST "p2address"
 #define PORT 8787
 
+void send_fd_dgram(int* usfd,int sendFd){
+	*usfd = socket(AF_UNIX,SOCK_DGRAM,0);
+	struct sockaddr_un addr;
+	memset(&addr,0,sizeof addr);
+	addr.sun_family=AF_UNIX;
+	strncpy(addr.sun_path,PATH,sizeof addr.sun_path - 1);
+	if(remove(PATH)==-1 && errno != ENOENT){
+		perror("remove ");
+	}
+	if(bind(*usfd,(struct sockaddr*)&addr,sizeof addr)==-1){
+		perror("bind ");
+	}
+	struct iovec e = {NULL,0};
+	char cmsg[CMSG_SPACE(sizeof(int))];
+	struct sockaddr_un destaddr;
+	memset(&destaddr,0,sizeof destaddr);
+	destaddr.sun_family=AF_UNIX;
+	strncpy(destaddr.sun_path,DEST,sizeof destaddr.sun_path - 1);
+	struct msghdr m = {(void*)&destaddr, sizeof(destaddr), &e, 1, cmsg, sizeof(cmsg), 0};
+	struct cmsghdr *c = CMSG_FIRSTHDR(&m);
+	c->cmsg_level = SOL_SOCKET;
+	c->cmsg_type = SCM_RIGHTS;
+	c->cmsg_len = CMSG_LEN(sizeof(int));
+	*(int*)CMSG_DATA(c) = sendFd;
+	sendmsg(*usfd, &m, 0);
+	perror("send ");
+}
+
+void send_fd_stream(int* nusfd,int sendFd){
+	struct iovec e[1];
+	char buff[1024];
+	buff[0]='F';
+	buff[1]='\0';
+	e[0].iov_base=buff;
+	e[0].iov_len=sizeof buff;
+	//struct iovec e={NULL,0};
+	char cmsg[CMSG_SPACE(sizeof(int))];
+	struct msghdr m = {NULL, 0, e, 1, cmsg, sizeof(cmsg), 0};
+	struct cmsghdr *c = CMSG_FIRSTHDR(&m);
+	c->cmsg_level = SOL_SOCKET;
+	c->cmsg_type = SCM_RIGHTS;
+	c->cmsg_len = CMSG_LEN(sizeof(int));
+	*(int*)CMSG_DATA(c) = sendFd;
+	sendmsg(*nusfd, &m, 0);
+	perror("send ");
+}
+
 int main(){
 	int sfd=socket(AF_INET,SOCK_STREAM,0);
 	struct sockaddr_in serveraddr;
@@ -25,7 +72,14 @@ int main(){
 	if(nsfd==-1){
 		perror("accept ");
 	}
-	int usfd=socket(AF_UNIX,SOCK_DGRAM,0);
+	int usfd;
+	send_fd_dgram(&usfd,nsfd);
+	char buff[1024];
+	int sz = recvfrom(usfd,buff,sizeof buff,0,NULL,NULL);
+	buff[sz]='\0';
+	printf("rcvd - %s",buff);
+	//through stream socket - fd passing
+	usfd = socket(AF_UNIX,SOCK_STREAM,0);
 	struct sockaddr_un addr;
 	memset(&addr,0,sizeof addr);
 	addr.sun_family=AF_UNIX;
@@ -36,22 +90,10 @@ int main(){
 	if(bind(usfd,(struct sockaddr*)&addr,sizeof addr)==-1){
 		perror("bind ");
 	}
-	struct iovec e = {NULL,0};
-	char cmsg[CMSG_SPACE(sizeof(int))];
-	struct sockaddr_un destaddr;
-	memset(&destaddr,0,sizeof destaddr);
-	destaddr.sun_family=AF_UNIX;
-	strncpy(destaddr.sun_path,DEST,sizeof destaddr.sun_path - 1);
-	struct msghdr m = {(void*)&destaddr, sizeof(destaddr), &e, 1, cmsg, sizeof(cmsg), 0};
-	struct cmsghdr *c = CMSG_FIRSTHDR(&m);
-	c->cmsg_level = SOL_SOCKET;
-	c->cmsg_type = SCM_RIGHTS;
-	c->cmsg_len = CMSG_LEN(sizeof(int));
-	*(int*)CMSG_DATA(c) = nsfd;
-	sendmsg(usfd, &m, 0);
-	perror("send ");
-	char buff[1024];
-	int sz = recvfrom(usfd,buff,sizeof buff,0,NULL,NULL);
-	buff[sz]='\0';
-	printf("rcvd - %s",buff);
+	listen(usfd,1);
+	printf("server ready\n");
+	fflush(stdout);
+	int nusfd=accept(usfd,NULL,NULL);
+	perror("accept ");
+	send_fd_stream(&nusfd,nsfd);
 }
--- UNIX_domain_sockets/Fd_passing/p2.c
+++ UNIX_domain_sockets/Fd_passing/p2.c
@@ -7,9 +7,10 @@
 #include<fcntl.h>
 #define PATH "p2address"
 #define DEST "p1address"
+#define SERVER "p1address"
 
-int main(){
-	int usfd=socket(AF_UNIX,SOCK_DGRAM,0);
+void recv_fd_dgram(int* usfd,int* recvFd){
+	*usfd = socket(AF_UNIX,SOCK_DGRAM,0);
 	struct sockaddr_un addr;
 	memset(&addr,0,sizeof addr);
 	addr.sun_family=AF_UNIX;
@@ -17,23 +18,50 @@ int main(){
 	if(remove(PATH)==-1 && errno != ENOENT){
 		perror("remove ");
 	}
-	if(bind(usfd,(struct sockaddr*)&addr,sizeof addr)==-1){
+	if(bind(*usfd,(struct sockaddr*)&addr,sizeof addr)==-1){
 		perror("bind ");
 	}
 	char buf[512];
 	struct iovec e = {buf, 512};
 	char cmsg[CMSG_SPACE(sizeof(int))];
 	struct msghdr m = {NULL, 0, &e, 1, cmsg, sizeof(cmsg), 0};
-	int n = recvmsg(usfd, &m, 0);
-	printf("Receive: %d\n", n);
+	int n = recvmsg(*usfd, &m, 0);
+	perror("Receive");
 	struct cmsghdr *c = CMSG_FIRSTHDR(&m);
-	int fd = *(int*)CMSG_DATA(c);
-	printf("fd is %i",fd);
+	*recvFd = *(int*)CMSG_DATA(c);
+	printf("recvFd is %i \n",*recvFd);
+	fflush(stdout);
+}
+
+void recv_fd_stream(int* usfd,int* recvFd){
+	*usfd = socket(AF_UNIX,SOCK_STREAM,0);
+	struct sockaddr_un serveraddr;
+	memset(&serveraddr,0,sizeof serveraddr);
+	serveraddr.sun_family=AF_UNIX;
+	strncpy(serveraddr.sun_path,SERVER,sizeof serveraddr.sun_path - 1);
+	int st=connect(*usfd,(struct sockaddr*)&serveraddr,sizeof serveraddr);
+	perror("connect ");
+	char buf[512];
+	struct iovec e = {NULL,0};//{buf, 512};
+	char cmsg[CMSG_SPACE(sizeof(int))];
+	struct msghdr m = {NULL, 0, &e, 1, cmsg, sizeof(cmsg), 0};
+	int n = recvmsg(*usfd, &m, 0);
+	perror("Receive");
+	struct cmsghdr *c = CMSG_FIRSTHDR(&m);
+	*recvFd = *(int*)CMSG_DATA(c);
+	printf("recvFd is %i \n",*recvFd);
+	fflush(stdout);
+}
+
+int main(){
+	int usfd;
+	int fd;
+	recv_fd_dgram(&usfd,&fd);
 	char buff[1024];
 	int sz = read(fd,buff,sizeof buff);
 	perror("read ");
 	buff[sz]='\0';
-	printf("read %s \n",buff);
+	printf("read from fd : %s \n",buff);
 	fflush(stdout);
 	struct sockaddr_un destaddr;
 	memset(&destaddr,0,sizeof destaddr);
@@ -41,4 +69,9 @@ int main(){
 	strncpy(destaddr.sun_path,DEST,sizeof destaddr.sun_path - 1);
 	sendto(usfd,buff,sz,0,(struct sockaddr*)&destaddr,sizeof destaddr);
 	perror("sendto ");
+	//recving fd through stream sockets
+	int fd2;
+	int usfd2;
+	getchar();
+	recv_fd_stream(&usfd2,&fd2);
 }
